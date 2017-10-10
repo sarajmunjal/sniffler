7f8a700877962574ed313e35e8d53d104ebd6f2a
Saraj Munjal <saraj.munjal@gmail.com>
2017-10-10 03:02:56 -0400

    HW2 intermediate state: basic packet sniffing and args input works

---
+++ src/main.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <pcap.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "sniffer.h"
+
+#define SIZE_ETHERNET 14
+
+void print_payload(u_char *payload, int size_payload) {
+    printf("Payload(size:%d) : %s\n", size_payload, payload);
+}
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+
+    static int count = 1;                   /* packet counter */
+    FILE *fp = (FILE *) args;
+    /* declare pointers to packet headers */
+    const ethernet_header_t *ethernet_header;  /* The ethernet header [1] */
+    const ip_header_t *ip;              /* The IP header */
+    const tcp_header_t *tcp;            /* The TCP header */
+    u_char *payload;                    /* Packet payload */
+
+    int size_ip;
+    int size_tcp;
+    int size_payload;
+
+    fprintf(fp, "\nPacket number %d:\n", count);
+    count++;
+
+    /* define ethernet header */
+    ethernet_header = (ethernet_header_t *) (packet);
+
+    /* define/compute ip header offset */
+    ip = (ip_header_t *) (packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip) * 4;
+    if (size_ip < 20) {
+        fprintf(fp, "   * Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+
+    /* print source and destination IP addresses */
+    fprintf(fp, "       From: %s\n", inet_ntoa(ip->ip_src));
+    fprintf(fp, "         To: %s\n", inet_ntoa(ip->ip_dst));
+
+    /* determine protocol */
+    switch (ip->ip_p) {
+        case IPPROTO_TCP:
+            fprintf(fp, "   Protocol: TCP\n");
+            break;
+        case IPPROTO_UDP:
+            fprintf(fp, "   Protocol: UDP\n");
+            return;
+        case IPPROTO_ICMP:
+            fprintf(fp, "   Protocol: ICMP\n");
+            return;
+        case IPPROTO_IP:
+            fprintf(fp, "   Protocol: IP\n");
+            return;
+        default:
+            fprintf(fp, "   Protocol: Other\n");
+            return;
+    }
+
+    /*
+     *  OK, this packet is TCP.
+     */
+
+    /* define/compute tcp header offset */
+    tcp = (tcp_header_t *) (packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp) * 4;
+    if (size_tcp < 20) {
+        fprintf(fp, "   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+
+    printf("   Src port: %d\n", ntohs(tcp->th_sport));
+    printf("   Dst port: %d\n", ntohs(tcp->th_dport));
+
+    /* define/compute tcp payload (segment) offset */
+    payload = (u_char *) (packet + SIZE_ETHERNET + size_ip + size_tcp);
+
+    /* compute tcp payload (segment) size */
+    size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+
+    /*
+     * Print payload data; it might be binary, so don't just
+     * treat it as a string.
+     */
+    if (size_payload > 0) {
+        fprintf(fp, "   Payload (%d bytes):\n", size_payload);
+        print_payload(payload, size_payload);
+    }
+
+    return;
+}
+
+custom_args parse_cli_arguments(int argc, char **argv) {
+    custom_args *args = (custom_args *) malloc(sizeof(custom_args));
+    int index;
+    int c;
+    opterr = 0;
+    while ((c = getopt(argc, argv, "i:r:s:")) != -1) {
+        switch (c) {
+            case 'i':
+                args->interface_name = optarg;
+                break;
+            case 'r':
+                args->input_file_name = optarg;
+                break;
+            case 's':
+                args->payload_search_string = optarg;
+                break;
+            case '?':
+                if (optopt == 'i' || optopt == 'r' || optopt == 's') {
+                    fprintf(stderr, "Option -%c requires an argument. Char : %c\n", optopt, c);
+                } else if (isprint(optopt)) {
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                } else {
+                    fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+                }
+            default:
+                abort();
+        }
+    }
+
+
+    printf("interface = %s, file_name = %s, string = %s\n",
+           args->interface_name, args->input_file_name, args->payload_search_string);
+
+    args->expression = argv[index];
+    return *args;
+}
+
+int main(int argc, char **argv) {
+    parse_cli_arguments(argc, argv);    
+    char errbuf[PCAP_ERRBUF_SIZE];
+    char *device = pcap_lookupdev(errbuf);
+    if (device == NULL) {
+        std::cout << "Error occurred:" << errbuf << std::endl;
+    } else {
+        std::cout << "Device found: " << device << std::endl;
+    }
+    pcap_t *session = pcap_open_live(device, BUFSIZ, 1, 1000, errbuf);
+    if (session == NULL) {
+        printf("Failed to open session for device:%s", device);
+        return 2;
+    }
+    struct pcap_pkthdr header;
+//    const u_char* packet = pcap_next(session, &header);
+    pcap_next(session, &header);
+    /* Print its length */
+    printf("Jacked a packet with length of [%d]\n", header.len);
+    FILE *fp = stdout;
+    pcap_loop(session, 100, got_packet, (u_char *) fp);
+    pcap_close(session);
+    return 0;
+}
---
+++ src/sniffer.h
@@ -0,0 +1,86 @@
+//
+// Created by Saraj Munjal on 10/2/17.
+//
+#include <sys/types.h>
+#include <arpa/inet.h>
+#ifndef HW2_SNIFFER_H
+#define HW2_SNIFFER_H
+
+
+#define ETHER_ADDR_LEN	6
+
+/* Ethernet header */
+typedef struct ethernet_header {
+    u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+    u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+    u_short ether_type; /* IP? ARP? RARP? etc */
+} ethernet_header_t;
+
+/* IP header */
+typedef struct ip_header {
+    u_char ip_vhl;		/* version << 4 | header length >> 2 */
+    u_char ip_tos;		/* type of service */
+    u_short ip_len;		/* total length */
+    u_short ip_id;		/* identification */
+    u_short ip_off;		/* fragment offset field */
+#define IP_RF 0x8000		/* reserved fragment flag */
+#define IP_DF 0x4000		/* dont fragment flag */
+#define IP_MF 0x2000		/* more fragments flag */
+#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
+    u_char ip_ttl;		/* time to live */
+    u_char ip_p;		/* protocol */
+    u_short ip_sum;		/* checksum */
+    struct in_addr ip_src,ip_dst; /* source and dest address */
+} ip_header_t;
+
+
+#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)		(((ip)->ip_vhl) >> 4)
+
+/* TCP header */
+typedef u_int tcp_seq;
+
+typedef struct tcp_header {
+    u_short th_sport;	/* source port */
+    u_short th_dport;	/* destination port */
+    tcp_seq th_seq;		/* sequence number */
+    tcp_seq th_ack;		/* acknowledgement number */
+    u_char th_offx2;	/* data offset, rsvd */
+#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
+    u_char th_flags;
+#define TH_FIN 0x01
+#define TH_SYN 0x02
+#define TH_RST 0x04
+#define TH_PUSH 0x08
+#define TH_ACK 0x10
+#define TH_URG 0x20
+#define TH_ECE 0x40
+#define TH_CWR 0x80
+#define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short th_win;		/* window */
+    u_short th_sum;		/* checksum */
+    u_short th_urp;		/* urgent pointer */
+} tcp_header_t;
+
+typedef struct udp_header {
+    u_short th_sport;	/* source port */
+    u_short th_dport;	/* destination port */
+    u_char th_offx2;	/* data offset, rsvd */
+    u_short th_len;		/* window */
+    u_short th_sum;		/* checksum */
+} udp_header_t;
+
+typedef struct icmp_header {
+    u_char type;
+    u_char code;
+    u_short sum; // checksum
+    u_int roh; // rest of header
+} icmp_header_t;
+
+typedef struct custom_args {
+    char *interface_name;
+    char *input_file_name;
+    char *payload_search_string;
+    char *expression;
+} custom_args;
+#endif //HW2_SNIFFER_H
